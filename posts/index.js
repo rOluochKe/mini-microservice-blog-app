3e37c841ca235c24e7486b8ad66a8394bf6507e0
Raymond Oluoch <rasholuoch@gmail.com>
2023-07-18 18:21:48 +0300

    finished orchestration

--- posts/index.js
+++ posts/index.js
@@ -14,8 +14,8 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", async (req, res) => {
-  const id = randomBytes(4).toString("hex");
+app.post('/posts/create', async (req, res) => {
+  const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
   posts[id] = {
